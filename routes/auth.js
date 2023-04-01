694db321ebaa8106eee0705a2fcc1aa78c613117
Pankaj Pandit <pankajpandit1215@gmail.com>
2023-04-01 18:59:40 +0530

    added middleware to avoid getting access to insider of website without logging in.

--- routes/auth.js
+++ routes/auth.js
@@ -18,9 +18,13 @@ router.get(
 
 //@desc    logout user
 //routes   /auth/logout
-router.get("/logout", (req, res) => {
-  req.logout();
-  res.redirect("/");
+router.get("/logout", (req, res, next) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
 });
 
 module.exports = router;
